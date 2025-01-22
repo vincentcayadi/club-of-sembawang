fb5db92b1e80f5938954ed00daae25da75163873
Vincent Cayadi <cayadivincent@gmail.com>
2025-01-22 08:26:09 +0800

    404 Update

--- src/app/(frontend)/not-found.tsx
+++ src/app/(frontend)/not-found.tsx
@@ -5,11 +5,14 @@ import { Button } from '@/components/ui/button'
 
 export default function NotFound() {
   return (
-    <div className="container py-28">
-      <div className="prose max-w-none">
-        <h1 style={{ marginBottom: 0 }}>404</h1>
-        <p className="mb-4">This page could not be found.</p>
-      </div>
+    <div className="h-[70vh] flex flex-col items-center justify-center w-3/4 mx-auto space-y-4 text-center">
+      <h1 className="text-9xl font-bold font-mono">404</h1>
+      <h2 className="text-3xl font-semibold capitalize">This page is not found</h2>
+      <p className="text-lg">
+        The page you are looking for might have been removed, had its name changed or is temporarily
+        unavailable.
+      </p>
+
       <Button asChild variant="default">
         <Link href="/">Go home</Link>
       </Button>
