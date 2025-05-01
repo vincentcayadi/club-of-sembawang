1fc40872a1d4d6a52ab36e6f49bae9639d390f59
vincentcayadi <cayadivincent@gmail.com>
2025-05-02 00:54:49 +0800

    fixed cta

--- src/blocks/CallToAction/Component.tsx
+++ src/blocks/CallToAction/Component.tsx
@@ -7,22 +7,20 @@ import { CMSLink } from '@/components/Link'
 
 export const CallToActionBlock: React.FC<CTABlockProps> = ({ links, richText }) => {
   return (
-    <section className={'w-full bg-gradient-to-r from-violet-500 to-blue-600'}>
+    <section
+      className={
+        'w-full bg-gradient-to-r from-violet-300 to-blue-400 dark:from-violet-600 dark:to-blue-700'
+      }
+    >
       <div className="mx-auto flex max-w-4xl flex-col items-center gap-6 px-6 py-20 text-center">
-        {richText && (
-          <RichText
-            content={richText}
-            enableGutter={false}
-            className="text-white dark:text-white"
-          />
-        )}
+        {richText && <RichText content={richText} enableGutter={false} />}
 
         <div className="">
           {(links || []).map(({ link }, i) => (
             <CMSLink
               key={i}
               size="lg"
-              className="rounded-full border-none bg-white px-6 py-3 text-blue-600 hover:bg-gray-200"
+              className="rounded-full border-none bg-white px-6 py-3 font-medium text-blue-600"
               {...link}
             />
           ))}
