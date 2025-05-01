955fb969a69b38b4a6e476856c20e6751ef514ac
vincentcayadi <cayadivincent@gmail.com>
2025-05-02 00:27:42 +0800

    CTA

--- src/blocks/CallToAction/Component.tsx
+++ src/blocks/CallToAction/Component.tsx
@@ -7,18 +7,27 @@ import { CMSLink } from '@/components/Link'
 
 export const CallToActionBlock: React.FC<CTABlockProps> = ({ links, richText }) => {
   return (
-    <div className="mx-auto flex max-w-4xl flex-col items-center justify-between gap-8 rounded-lg border border-border bg-gradient-to-r from-violet-500 to-blue-600 p-8 text-center text-neutral-100 md:flex-row md:text-left">
-      {richText && (
-        <div className="flex-1">
-          <RichText className="mb-0" content={richText} enableGutter={false} />
-        </div>
-      )}
+    <section className={'w-full bg-gradient-to-r from-violet-500 to-blue-600'}>
+      <div className="mx-auto flex max-w-4xl flex-col items-center gap-6 px-6 py-20 text-center">
+        {richText && (
+          <RichText
+            content={richText}
+            enableGutter={false}
+            className="text-white dark:text-white"
+          />
+        )}
 
-      <div className="flex flex-wrap justify-center gap-4 md:justify-end">
-        {(links || []).map(({ link }, i) => (
-          <CMSLink key={i} size="lg" {...link} />
-        ))}
+        <div className="">
+          {(links || []).map(({ link }, i) => (
+            <CMSLink
+              key={i}
+              size="lg"
+              className="rounded-full border-none bg-white px-6 py-3 text-blue-600 hover:bg-gray-200"
+              {...link}
+            />
+          ))}
+        </div>
       </div>
-    </div>
+    </section>
   )
 }
