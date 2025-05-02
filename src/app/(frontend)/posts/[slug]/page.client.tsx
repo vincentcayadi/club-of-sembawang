779face43da20330e588d55b200abc9356a866f3
vincentcayadi <cayadivincent@gmail.com>
2025-05-02 15:22:10 +0800

    bug

--- src/app/(frontend)/posts/[slug]/page.client.tsx
+++ src/app/(frontend)/posts/[slug]/page.client.tsx
@@ -1,14 +1,15 @@
 'use client'
-import { useHeaderTheme } from '@/providers/HeaderTheme'
-import React, { useEffect } from 'react'
+// import { useHeaderTheme } from '@/providers/HeaderTheme'
+// import React, { useEffect } from 'react'
+import React from "react"
 
 const PageClient: React.FC = () => {
-  /* Force the header to be dark mode while we have an image behind it */
-  const { setHeaderTheme } = useHeaderTheme()
+  // /* Force the header to be dark mode while we have an image behind it */
+  // const { setHeaderTheme } = useHeaderTheme()
 
-  useEffect(() => {
-    setHeaderTheme('dark')
-  }, [setHeaderTheme])
+  // useEffect(() => {
+  //   setHeaderTheme('dark')
+  // }, [setHeaderTheme])
   return <React.Fragment />
 }
 
