aae978b629f6eb00f43a84b0e5a4bb3da41a6db9
Vincent Cayadi <57314503+vincentcayadi@users.noreply.github.com>
2025-11-24 04:25:06 +0800

    fix

--- src/blocks/Quote/Component.tsx
+++ src/blocks/Quote/Component.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useEffect, useRef } from 'react'
+import React, { useEffect, useRef } from 'react'
 import { gsap } from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
 import { cn } from '@/lib/utils'
